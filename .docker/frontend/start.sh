6bfd32d105523fb051726a7dab1dd7710b0e2d61
Bart Różycki <bartlomiej.rozycki@impactsoft.pl>
2019-10-23 00:35:17 +0200

    fix: Solve nvm problem

--- .docker/frontend/start.sh
+++ .docker/frontend/start.sh
@@ -2,8 +2,12 @@
 
 cd /application
 
-npm install -g create-react-app
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install node
 
+npm install -g create-react-app
 npm install
 npm run build
 
